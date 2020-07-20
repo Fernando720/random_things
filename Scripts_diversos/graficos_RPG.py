61f1d0988e30378ac701b3d9c0456f90bfad47c0
fernando720 <fhac@cin.ufpe.br>
2020-07-20 13:59:02 -0300

    gráficos_RPG.py melhorado

--- Scripts_diversos/graficos_RPG.py
+++ Scripts_diversos/graficos_RPG.py
@@ -1,14 +1,15 @@
+import os
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 import pandas as pd
 
-global personagens
+global personagens     #deixando esta variável global por nenhum motivo aparente, mas acho que será importante (sei lá, tá dando no mesmo)
 #'Cinthria','Eduardo','Glar','Naeris','Torphin'
+
 personagens = ['Arya','Astrid','Welby']
 
 def adicionando_personagens():
-
 
     print('os personagens pré-definidos são: ' )
     print(personagens)
@@ -22,32 +23,37 @@ def adicionando_personagens():
     print(personagens)
     return personagens
 
+########################################################## INICIATIVA #########################################################################
 
-
-########## INICIATIVA ##############
 def ordem_iniciativa(lista_personagens):
 
     iniciativa = []
 
     count_2 = 0
-    while count_2 <len(lista_personagens):
-        iniciativa.append(int(input('Digite a iniciativa de ' + lista_personagens[count_2] + ': ')))
-        count_2+=1
+
+    while True:
+        try:
+            #loop para adicionar iniciativa de cada personagem
+            while count_2 <len(lista_personagens):
+                iniciativa.append(int(input('Digite a iniciativa de ' + lista_personagens[count_2] + ': ')))
+                count_2+=1
+            break
+
+        except:
+            print("Digitou algum número errado? Digite novamente.")
 
     #combinando as duas listas, ou seja, atribuindo a iniciativa ao personagem:
     combinacao = list(zip(lista_personagens,iniciativa))
 
-    #ordenando a fila
+    #ordenando a fila de forma decrescente
     fila = sorted(combinacao, key=lambda fila: fila[1],reverse=True)
 
-    # Arrumando um jeito mais bonito de printar a fila (depois implementar um GUI com tkinter, se pá)
+    # Arrumando um jeito mais bonito de printar a fila (enquanto não se tem um GUI)
     df_fila = pd.DataFrame(fila, columns=['Personagem', 'Iniciativa'],
                            index=[(x + 1) for x in range(len(lista_personagens))])
-    print(df_fila)
-
     return df_fila
 
-
+############################################################ COMBATE #############################################################################
 
 def combate(ordem_batalha):
 
@@ -78,19 +84,42 @@ def combate(ordem_batalha):
                 if turno == 1:
                     print('Vez de %s' % ordem_batalha['Personagem'][vez])
 
-                    dano_causado.append(int(input('quanto de dano %s causou neste turno? ' % ordem_batalha['Personagem'][vez])))
-                    dano_sofrido.append(int(input('quanto de dano %s sofreu neste turno? ' % ordem_batalha['Personagem'][vez])))
-                    cura_realizada.append(int(input('quanto de cura %s realizou neste turno? ' % ordem_batalha['Personagem'][vez])))
-                    controle_causado.append(int(input('quanto de controle %s causou neste turno? ' % ordem_batalha['Personagem'][vez])))
-                    dano_automitigado.append(int(input('quanto de dano %s automitigou nesta turno? ' % ordem_batalha['Personagem'][vez])))
+                    while True:
+                        try:
+                            dano_causado.append(int(input('quanto de dano %s causou neste turno? ' % ordem_batalha['Personagem'][vez])))
+                            dano_sofrido.append(int(input('quanto de dano %s sofreu neste turno? ' % ordem_batalha['Personagem'][vez])))
+                            cura_realizada.append(int(input('quanto de cura %s realizou neste turno? ' % ordem_batalha['Personagem'][vez])))
+                            controle_causado.append(int(input('quanto de controle %s causou neste turno? ' % ordem_batalha['Personagem'][vez])))
+                            dano_automitigado.append(int(input('quanto de dano %s automitigou nesta turno? ' % ordem_batalha['Personagem'][vez])))
+                            break
+
+                        except ValueError:
+                            print('Digitou algum número errado? Preencha novamente os dados do personagem.')
+                            try:
+                                dano_causado.pop(vez-1)
+                                dano_sofrido.pop(vez-1)
+                                cura_realizada.pop(vez-1)
+                                controle_causado.pop(vez-1)
+                                dano_automitigado.pop(vez-1)
+
+                            except IndexError:
+                                pass
+
 
                 else:
-                    print('Vez de %s' % ordem_batalha['Personagem'][vez])
-                    aux_dc = int(input('quanto de dano %s causou neste turno? ' %ordem_batalha['Personagem'][vez]))
-                    aux_ds = int(input('quanto de dano %s sofreu neste turno? ' % ordem_batalha['Personagem'][vez]))
-                    aux_cr = int(input('quanto de cura %s realizou neste turno? ' % ordem_batalha['Personagem'][vez]))
-                    aux_cc = int(input('quanto de controle %s causou neste turno? ' % ordem_batalha['Personagem'][vez]))
-                    aux_da = int(input('quanto de dano %s automitigou nesta turno? ' % ordem_batalha['Personagem'][vez]))
+                    while True:
+                        print('Vez de %s' % ordem_batalha['Personagem'][vez])
+                        try:
+
+                            aux_dc = int(input('quanto de dano %s causou neste turno? ' %ordem_batalha['Personagem'][vez]))
+                            aux_ds = int(input('quanto de dano %s sofreu neste turno? ' % ordem_batalha['Personagem'][vez]))
+                            aux_cr = int(input('quanto de cura %s realizou neste turno? ' % ordem_batalha['Personagem'][vez]))
+                            aux_cc = int(input('quanto de controle %s causou neste turno? ' % ordem_batalha['Personagem'][vez]))
+                            aux_da = int(input('quanto de dano %s automitigou nesta turno? ' % ordem_batalha['Personagem'][vez]))
+                            break
+
+                        except ValueError:
+                            print('Digitou algum número errado? Preencha novamente os dados do personagem.')
 
                     dano_causado[vez-1] += aux_dc
                     dano_sofrido[vez-1] += aux_ds
@@ -101,20 +130,75 @@ def combate(ordem_batalha):
                 vez += 1
 
         turno += 1
-        '''print(dano_causado)
-        print(dano_sofrido)
-        print(cura_realizada)
-        print(controle_causado)
-        print(dano_automitigado)'''
 
     return np.array([dano_causado, dano_sofrido, cura_realizada, controle_causado, dano_automitigado])
 
+#juntar os dados de combate e os dados dos jogadores numa só tabela
+def dataframing_everything(dados_combate,ordem_batalha):
 
+    df_dados = pd.DataFrame(ordem_batalha, columns=['Personagem','Iniciativa','dano causado', 'dano sofrido',
+                                                'cura realizada', 'controle causado', 'dano automitigado'])
+
+    df_dados['dano causado'] = dados_combate[0]
+    df_dados['dano sofrido'] = dados_combate[1]
+    df_dados['cura realizada'] = dados_combate[2]
+    df_dados['controle causado'] = dados_combate[3]
+    df_dados['dano automitigado'] = dados_combate[4]
+
+    tabela_dados = df_dados.drop(columns='Iniciativa')
+
+    return tabela_dados
+
+################################################################## GRÁFICOS #####################################################################
+def plotting(tabela_dados):
+
+    df = tabela_dados
+    #criando figuras
+    fig = plt.figure(1)
+    #fig2 = plt.figure(2)
+
+    #Specifies the geometry of the grid that a subplot can be placed in.
+    gs = gridspec.GridSpec(2,3,figure=fig)
+    #gs2 = gridspec.GridSpec(1,2,figure=fig2)
+
+    #criando primeiro subplot (Dano causado)
+    ax1 = fig.add_subplot(gs[0,0])
+    y1 = list(df['dano causado'])
+    ax1.barh(df['Personagem'], y1, color='orange')
+    plt.title('Dano causado por personagem')
+    for i, v in enumerate(y1):
+        ax1.text(v - 3, i, str(v), color='black', fontweight='bold')
+
+    #Criando os demais subplots (primeira linha os danos, segunda linha os demais)
+    ax2 = fig.add_subplot(gs[0,1])
+    ax2.barh(df['Personagem'], df['dano sofrido'],color='red')
+    plt.title('Dano sofrido por personagem')
+
+    ax3 = fig.add_subplot(gs[0,2])
+    ax3.barh(df['Personagem'], df['dano automitigado'],color='gray')
+    plt.title('Dano automitigado por personagem')
+
+    ax4 = fig.add_subplot(gs[1,0])
+    ax4.barh(df['Personagem'], df['cura realizada'],color='green')
+    plt.title('Cura realizada por personagem')
+
+    ax5 = fig.add_subplot(gs[1,2])
+    ax5.barh(df['Personagem'], df['controle causado'],color='blue')
+    plt.title('controle causado por personagem')
+
+    plt.show()
+
+########################################################### "FUNÇÃO PRINCIPAL" ##################################################################
 if __name__ == '__main__':
-    
+
     fila_turno_RPG = ordem_iniciativa(adicionando_personagens())
-    dados_de_combate = combate(fila_turno_RPG)
-    print(dados_de_combate)
+    print(fila_turno_RPG)
+    dados_combate = combate(fila_turno_RPG)
+    print(dados_combate)
+    tabela = dataframing_everything(dados_combate, fila_turno_RPG)
+    print(tabela)
+
+    plotting(tabela)
 
 
 
